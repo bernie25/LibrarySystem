ff9d788135c0090f3e48131e8abc35602ae89ffc
jonesc-10 <joneschrisog11@gmail.com>
2021-11-06 22:49:55 +0000

    Registration and login pages set up and functioning, logout function also implemented.

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     #path('user/', views.LibrarySystemView, name='user'),
     path('home/', views.index, name='index'),
     path('addBook/', views.addBook, name='addBook'),
+
+    path('', views.home, name="home"),
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
