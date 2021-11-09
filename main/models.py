3e2ed7c2af04eb8df748caec36c4c00718ba181a
Jessoflynn1 <93090036+Jessoflynn1@users.noreply.github.com>
2021-11-09 09:41:28 +0000

    Revert "Registration and login pages set up and functioning, logout function …"

--- main/models.py
+++ main/models.py
@@ -1,79 +1,23 @@
-# #Database layout
-
-# from django.db import models
-
-# class LibrarySystem(models.Model):
-#     book_name = models.CharField(max_length=200)
-
-#     def _str_(self):
-#         return self.title
-
-
-#         #Business logic
-#         # 
-#         #Design patterns, Added Value 
-#         #Factory method - Loan Book - instead of new
-
-# class Book(models.Model):
-#     book_name = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-    
-#     def _str_(self):
-#         return self.title
+#Database layout
 
 from django.db import models
 
-# Create your models here.
+class LibrarySystem(models.Model):
+    book_name = models.CharField(max_length=200)
 
-class Customer(models.Model):
-	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-	def __str__(self):
-		return self.name
+    def _str_(self):
+        return self.title
 
 
-class Tag(models.Model):
-	name = models.CharField(max_length=200, null=True)
+        #Business logic
+        # 
+        #Design patterns, Added Value 
+        #Factory method - Loan Book - instead of new
 
-	def __str__(self):
-		return self.name
+class Book(models.Model):
+    book_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
-class Product(models.Model):
-	CATEGORY = (
-			('Indoor', 'Indoor'),
-			('Out Door', 'Out Door'),
-			) 
-
-	name = models.CharField(max_length=200, null=True)
-	price = models.FloatField(null=True)
-	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True, blank=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	tags = models.ManyToManyField(Tag)
-
-	def __str__(self):
-		return self.name
-
-class Order(models.Model):
-	STATUS = (
-			('Pending', 'Pending'),
-			('Out for delivery', 'Out for delivery'),
-			('Delivered', 'Delivered'),
-			)
-
-	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
-	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	note = models.CharField(max_length=1000, null=True)
-
-	def __str__(self):
-		return self.product.name
-
-
-
-	
+    
+    def _str_(self):
+        return self.title
--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,4 @@ urlpatterns = [
     #path('user/', views.LibrarySystemView, name='user'),
     path('home/', views.index, name='index'),
     path('addBook/', views.addBook, name='addBook'),
-
-    path('', views.home, name="home"),
-    path('register/', views.registerPage, name="register"),
-	path('login/', views.loginPage, name="login"),  
-	path('logout/', views.logoutUser, name="logout"),
 ]
