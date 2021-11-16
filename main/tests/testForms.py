0c19bc46ad33cd41bf749875a00dc281903d62b8
Jessoflynn1 <jessoflynn1@gmail.com>
2021-11-16 21:01:40 +0000

    tests

---
+++ main/tests/testForms.py
@@ -0,0 +1,12 @@
+from forms.forms import SignUpForm
+
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        self.assertTrue(form.fields['first_name'].label is None)
+        self.assertTrue(form.fields['last_name'].label is None)
+        self.assertTrue(form.fields['phone'].label is None)
+        self.assertTrue(form.fields['email'].label is None)
+
+    #Test sign up success
+
+    #Test sign up failure
