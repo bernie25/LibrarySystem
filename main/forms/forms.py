796d8130db10083a81a39c66cb942822170fe422
Jessoflynn1 <jessoflynn1@gmail.com>
2021-11-16 20:11:17 +0000

    forms.py

--- main/forms/forms.py
+++ main/forms/forms.py
@@ -16,3 +16,13 @@ class CreateUserForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
+#Signup form
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, help_text='Last Name')
+    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    email = forms.EmailField(max_length=150, help_text='Email')
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name',
+'email', 'password1', 'password2',)
+
