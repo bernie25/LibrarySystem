577d7d1c0146ea3df03d8c64380dba3769c01d3e
Jessoflynn1 <jessoflynn1@gmail.com>
2021-11-18 09:16:43 +0000

    tests#2 not ran it yet

--- main/tests/testForms.py
+++ main/tests/testForms.py
@@ -2,12 +2,13 @@ from forms.forms import SignUpForm
 from django.test import TestCase
 
 
+#Don't know if this works 
 class SignUpFormTest(TestCase):
     def test_form_has_fields(self):
-        self.assertTrue(form.fields['first_name'].label is None)
-        self.assertTrue(form.fields['last_name'].label is None)
-        self.assertTrue(form.fields['phone'].label is None)
-        self.assertTrue(form.fields['email'].label is None)
+        #self.assertTrue(forms.fields['first_name'].label is None)
+        #self.assertTrue(form.fields['last_name'].label is None)
+        #self.assertTrue(form.fields['phone'].label is None)
+        #self.assertTrue(form.fields['email'].label is None)
 
     #Test sign up success
 
