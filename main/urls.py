0bee62bd756bf77c9853c5f48c24b451e8681ac5
jessoflynn1 <jessoflynn1@gmail.com>
2021-12-08 15:57:01 +0000

    Updates 08/12

--- main/urls.py
+++ main/urls.py
@@ -1,34 +1,38 @@
+#django imports
 from django.urls import path
-from django.urls import path
-
-#Forms Import
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+#forms
 from main.forms.bookform import FormBook
 from main.forms.bookingForm import BookingForm
-
-#Models import
-from main.models import Book, BookingDetails
-
-#Services import
+#models
+from main.models import Book
+from main.models import BookingDetails
+#services
 from main.services.book import CreateRoomService
-from main.services.booking.booking import CreateBookingService
-
-#Views import
-from main.views.views import libraryView
+#views
 from main.views import views
+from main.views.views import libraryView
 from main.views.book import BookCreateView
-from main.views.booking import bookingView, createBookingView
-from main.views.addBook import addBook
-from main.views.homepage import homePage
-from main.views.signup import signup
 from main.forms.bookingForm import BookingForm
 from main.models import BookingDetails
 from main.services.booking.booking import CreateBookingService
 from main.views.booking import bookingView, createBookingView
 from main.views.addBook import addBook
 from main.views.homepage import homePage
+from main.views.booking import bookingView, createBookingView
+from main.views.addBook import addBook
+from main.views.homepage import homePage
+from main.views.signup import signup
+from main.views.library import library
+
 
 urlpatterns = [
     #path('', views.home, name="home"),
+    path('', views.home, name="home"),
     #path('register/', views.registerPage, name="register"),
 	#path('login/', views.loginPage, name="login"),  
 	#path('logout/', views.logoutUser, name="logout"),
@@ -41,18 +45,7 @@ urlpatterns = [
                             model=Book,
                             template_name='../templates/addBook.html', success_url='.'),
                             name='add-book'),
-
-    #Homepage
-    path('', homePage, name='homePage'),
-
-    #Add a new book
-    path('addBook/', addBook, name='addBook'),
-    
-    #View Booking
-    path('booking/', bookingView, name='booking'),
-
-    #Create a new booking
-    path('booking/create/', 
+     path('booking/create/', 
             createBookingView.as_view(service_class= CreateBookingService,
             form_class=BookingForm,
             model=BookingDetails,
@@ -61,4 +54,19 @@ urlpatterns = [
             name='booking-create'),
 
     path('library/', libraryView),
+    path('home/', homePage, name='homePage'),
+    path('addBook/', addBook, name='addBook'),
+    path('booking/', bookingView, name='booking'),
+    path('booking/create/', createBookingView, name='createBooking'),
+    path('library', library, name='library'),
+    path('signup', signup, name='signup'),
+
+        # path('booking/create/', 
+            #     createBookingView.as_view(service_class= AbstractCreateBookingService,
+            #     form_class=BookingForm,
+            #     model=BookingDetails,
+            #     template_name='main/templates/booking.html',
+            #     success_url='.'
+            #     ), 
+            #     name='create/booking'),
 ]
