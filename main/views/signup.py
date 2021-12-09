ccf467c67bcca745126ffb7610c67a99072bb46d
jessoflynn1 <jessoflynn1@gmail.com>
2021-12-09 15:14:44 +0000

    Signup Updates 09/12/2021

--- main/views/signup.py
+++ main/views/signup.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.forms.signupform import SignUpForm
+from main.models import Student
 from main.views.userfactory import UserFactory
+from django.contrib.auth import authenticate, login
+
 
 #Signup basic registration function
 def signup(request):
-    factory = UserFactory()
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-        user = form.save()
-        factory.createuser(form, user)
-        # user can't login until link confirmed
-        user.save()
+    if request.method == 'POST':
+        factory = UserFactory()
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            factory.createuser(form, user)
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            # login user after signing up
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            # redirect user to home page
+            return redirect('home')
     else:
-            form = SignUpForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
