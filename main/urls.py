580f18ecb58eecc537be326e72550d36ed7650c2
jessoflynn1 <jessoflynn1@gmail.com>
2021-12-14 21:03:15 +0000

    Final Updates

--- main/urls.py
+++ main/urls.py
@@ -1,39 +1,46 @@
+#django imports
 from django.urls import path
-from django.urls import path
-
-#Forms Import
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+#forms
 from main.forms.bookform import FormBook
 from main.forms.bookingForm import BookingForm
-
-#Models import
-from main.models import Book, BookingDetails
-
-#Services import
+#models
+from main.models import Book
+from main.models import BookingDetails
+#services
 from main.services.book import CreateRoomService
 from main.services.booking.booking import CreateBookingService
-
-#Views import
-from main.views.views import libraryView
-from main.views import views
+#views
+from . import views
+from main.views.views import *
 from main.views.book import BookCreateView
-from main.views.booking import bookingView, createBookingView
-from main.views.addBook import addBook
-from main.views.homepage import homePage
-from main.views.signup import signup
 from main.forms.bookingForm import BookingForm
 from main.models import BookingDetails
-from main.services.booking.booking import CreateBookingService
+from main.views.views import libraryView
 from main.views.booking import bookingView, createBookingView
 from main.views.addBook import addBook
 from main.views.homepage import homePage
+from main.views.booking import bookingView, createBookingView
+from main.views.addBook import addBook
+from main.views.homepage import homePage
+from main.views.library import library
+from main.views.signup import signup
+
+from main.views.views import *
+from main.views import views
+from django.contrib.auth import views as auth_views
+from main.views.views import logout
 
 urlpatterns = [
     #path('', views.home, name="home"),
-    #path('register/', views.registerPage, name="register"),
-	#path('login/', views.loginPage, name="login"),  
-	#path('logout/', views.logoutUser, name="logout"),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.home, name="home"),
     path('reqbook/', views.reqbook, name="reqbook"),
-    path('signup/', views.signup, name="signup"),
     path('book/new/',
         BookCreateView.as_view(
                             service_class=CreateRoomService,
@@ -41,24 +48,43 @@ urlpatterns = [
                             model=Book,
                             template_name='../templates/addBook.html', success_url='.'),
                             name='add-book'),
-
-    #Homepage
-    path('', homePage, name='homePage'),
-
-    #Add a new book
-    path('addBook/', addBook, name='addBook'),
-    
-    #View Booking
-    path('booking/', bookingView, name='booking'),
-
-    #Create a new booking
-    path('booking/create/', 
-            createBookingView.as_view(service_class= CreateBookingService,
-            form_class=BookingForm,
-            model=BookingDetails,
-            template_name='createBooking.html',
-            success_url='.'), 
-            name='booking-create'),
+     path('booking/create/', 
+        createBookingView.as_view(
+                            service_class= CreateBookingService,
+                            form_class=BookingForm,
+                            model=BookingDetails,
+                            template_name='createBooking.html',
+                            success_url='.'), 
+                            name='booking-create'),
 
     path('library/', libraryView),
+    path('home/', homePage, name='homePage'),
+    path('addBook/', addBook, name='addBook'),
+    path('booking/', bookingView, name='booking'),
+    path('booking/create/', createBookingView, name='createBooking'),
+    path('library', library, name='library'),
+    path('signup/', signup, name='signup'),
+    path('book_form/', bookform, name='book_form'),
+    #url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate,
+        #name='activate'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', logout, name='logout'),
+        # path('booking/create/', 
+            #     createBookingView.as_view(service_class= AbstractCreateBookingService,
+            #     form_class=BookingForm,
+            #     model=BookingDetails,
+            #     template_name='main/templates/booking.html',
+            #     success_url='.'
+            #     ), 
+            #     name='create/booking'),
+    ##############################################################################
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('bookdetail/', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 ]
